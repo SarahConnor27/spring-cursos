3c996720260b45f66e3c7e7031e36d3316a915f7
SarahConnor27 <scexterminio@gmail.com>
2019-11-20 15:43:18 -0300

    inicial

---
+++ src/main/java/br/com/curso/ExemploSpringBoot.java
@@ -0,0 +1,14 @@
+package br.com.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSpringBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExemploSpringBoot.class, args);
+    }
+
+}
+
